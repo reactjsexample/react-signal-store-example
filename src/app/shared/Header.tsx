2cba9761b1a8291d818985cc4ee0c6627ca3e011
reactjsexample <info@reactjsexample.com>
2026-02-07 18:33:20 -0500

    style header, horizontal nav

--- src/app/shared/Header.tsx
+++ src/app/shared/Header.tsx
@@ -6,27 +6,27 @@ function MyHeader() {
     useSignals(); // re-render view when signals change
 
     return (
-        <div className={"items-center bg-[aquamarine] flex w-full p-4 gap-4"}>
+        <div className={"items-center bg-[aquamarine] flex flex-col w-full p-4 gap-4"}>
             <div>
-            <h1 className="text-4xl font-bold">react-signal-store-example</h1>
+                <h1 className="text-4xl font-bold">react-signal-store-example</h1>
             </div>
             <div>
-            <nav className="flex flex-col items-center">
-                <ul>
-                    <li className={`${AppStore.selectedPage.value === 'home' ? 'active' : ''}`}>
-                        <Link className="p-2" to="/">Home</Link>
-                    </li>
-                    <li className={`${AppStore.selectedPage.value === 'user' ? 'active' : ''}`}>
-                    <Link className="p-2" to="/user">Users</Link>
-                    </li>
-                    <li className={`${AppStore.selectedPage.value === 'post' ? 'active' : ''}`}>
-                    <Link className="p-2" to="/post">Posts</Link>
-                    </li>
-                    <li className={`${AppStore.selectedPage.value === 'post-edit' ? 'active' : ''}`}>
-                        <Link className="p-2" to="/post-edit">Post Edit</Link>
-                    </li>
-                </ul>
-            </nav>
+                <nav>
+                    <ul className="flex items-center">
+                        <li className={`${AppStore.selectedPage.value === 'home' ? 'active' : ''}`}>
+                            <Link className="p-2" to="/">Home</Link>
+                        </li>
+                        <li className={`${AppStore.selectedPage.value === 'user' ? 'active' : ''}`}>
+                            <Link className="p-2" to="/user">Users</Link>
+                        </li>
+                        <li className={`${AppStore.selectedPage.value === 'post' ? 'active' : ''}`}>
+                            <Link className="p-2" to="/post">Posts</Link>
+                        </li>
+                        <li className={`${AppStore.selectedPage.value === 'post-edit' ? 'active' : ''}`}>
+                            <Link className="p-2" to="/post-edit">Post Edit</Link>
+                        </li>
+                    </ul>
+                </nav>
             </div>
         </div>
     )
