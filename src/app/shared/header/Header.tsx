448af0ac2924d3dbbf751a66abdb4bb166f54677
reactjsexample <info@reactjsexample.com>
2026-02-08 18:52:48 -0500

    create NavBar

--- src/app/shared/header/Header.tsx
+++ src/app/shared/header/Header.tsx
@@ -1,5 +1,5 @@
 import * as AppStore from "../../appStore.tsx";
-import {Link} from "react-router";
+import {NavBar} from "../nav-bar/NavBar.tsx";
 import {useSignals} from "@preact/signals-react/runtime";
 
 function Header() {
@@ -10,24 +10,7 @@ function Header() {
             <div>
                 <h1 className="text-4xl font-bold">react-signal-store-example</h1>
             </div>
-            <div>
-                <nav>
-                    <ul className="flex items-center">
-                        <li className={`${AppStore.selectedPage.value === 'home' ? 'active' : ''}`}>
-                            <Link className="p-2" to="/">Home</Link>
-                        </li>
-                        <li className={`${AppStore.selectedPage.value === 'user' ? 'active' : ''}`}>
-                            <Link className="p-2" to="/user">Users</Link>
-                        </li>
-                        <li className={`${AppStore.selectedPage.value === 'post' ? 'active' : ''}`}>
-                            <Link className="p-2" to="/post">Posts</Link>
-                        </li>
-                        <li className={`${AppStore.selectedPage.value === 'post-edit' ? 'active' : ''}`}>
-                            <Link className="p-2" to="/post-edit">Post Edit</Link>
-                        </li>
-                    </ul>
-                </nav>
-            </div>
+            <NavBar selectedPage={AppStore.selectedPage.value} />
         </div>
     )
 }
---
+++ src/app/shared/nav-bar/NavBar.tsx
@@ -0,0 +1,27 @@
+import type {Page} from "../../appTypes.tsx";
+import {Link} from "react-router";
+
+type NavBarProps = {
+    selectedPage: Page;
+}
+
+export function NavBar({selectedPage}: NavBarProps) {
+    return (
+        <nav>
+            <ul className="flex items-center">
+                <li className={`${selectedPage === 'home' ? 'active' : ''}`}>
+                    <Link className="p-2" to="/">Home</Link>
+                </li>
+                <li className={`${selectedPage === 'user' ? 'active' : ''}`}>
+                    <Link className="p-2" to="/user">Users</Link>
+                </li>
+                <li className={`${selectedPage === 'post' ? 'active' : ''}`}>
+                    <Link className="p-2" to="/post">Posts</Link>
+                </li>
+                <li className={`${selectedPage === 'post-edit' ? 'active' : ''}`}>
+                    <Link className="p-2" to="/post-edit">Post Edit</Link>
+                </li>
+            </ul>
+        </nav>
+    )
+}
