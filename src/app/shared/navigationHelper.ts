9fad7324da2d9527c505a87ba08265c511d41286
reactjsexample <info@reactjsexample.com>
2026-02-08 17:50:34 -0500

    Remove unused file

--- src/app/shared/navigationHelper.ts
+++
@@ -1,5 +0,0 @@
-// Custom history object to allow navigation from anywhere in the app
-export const navigationHelper = {
-  navigate: null,
-  location: null, // you can also add location if needed
-};
