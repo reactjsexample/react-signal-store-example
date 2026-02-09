11b49953af50aa6c44814173421b352497195d5f
reactjsexample <info@reactjsexample.com>
2026-02-08 19:31:07 -0500

    styles for NavBar

--- src/app/shared/nav-bar/NavBar.tsx
+++ src/app/shared/nav-bar/NavBar.tsx
@@ -1,5 +1,6 @@
 import type {Page} from "../../appTypes.tsx";
 import {Link} from "react-router";
+import "./navBar.css";
 
 type NavBarProps = {
     selectedPage: Page;
@@ -10,16 +11,16 @@ export function NavBar({selectedPage}: NavBarProps) {
         <nav>
             <ul className="flex items-center">
                 <li className={`${selectedPage === 'home' ? 'active' : ''}`}>
-                    <Link className="p-2" to="/">Home</Link>
+                    <Link className="nav-link" to="/">Home</Link>
                 </li>
                 <li className={`${selectedPage === 'user' ? 'active' : ''}`}>
-                    <Link className="p-2" to="/user">Users</Link>
+                    <Link className="nav-link" to="/user">Users</Link>
                 </li>
                 <li className={`${selectedPage === 'post' ? 'active' : ''}`}>
-                    <Link className="p-2" to="/post">Posts</Link>
+                    <Link className="nav-link" to="/post">Posts</Link>
                 </li>
                 <li className={`${selectedPage === 'post-edit' ? 'active' : ''}`}>
-                    <Link className="p-2" to="/post-edit">Post Edit</Link>
+                    <Link className="nav-link" to="/post-edit">Post Edit</Link>
                 </li>
             </ul>
         </nav>
